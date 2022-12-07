9e0a1a88e8b15931f5eba89fa2c4281f92af82a5
Cazier92 <bryce.cazier@gmail.com>
2022-12-07 15:32:28 -0800

    prevent empty items being added to ul

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ const listEl = document.getElementById('todo-list')
 // console.log(inputEl, buttonEl, listEl)
 
 btn.addEventListener('click', function() {
+    if (inp.value !== '') {
     const li = document.createElement('li')
     li.textContent = inp.value
     listEl.appendChild(li)
     inp.value = ''
+    }
 })
