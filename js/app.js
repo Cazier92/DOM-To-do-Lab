953c416760237725a9c89881181cda6fe56fbdac
Cazier92 <bryce.cazier@gmail.com>
2022-12-07 15:21:01 -0800

    complete step 5

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 console.log('sanity check')
+
+const inputEl = document.getElementById('typeBox')
+const buttonEl = document.getElementById('submit-button')
+const listEl = document.getElementById('todo-list')
+
+// console.log(inputEl, buttonEl, listEl)
